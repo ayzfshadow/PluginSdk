2ed9c53314b1b9507f5f67c20efe2dbf605427a2
ayzf <ayzfshadow@qq.com>
2022-09-02 11:18:08 +0800

    20220831 版本

---
+++ app/src/main/java/ayzf/darkqq/qqbot/plugin/PluginBinderHandler.java
@@ -0,0 +1,33 @@
+package ayzf.darkqq.qqbot.plugin;
+
+import android.graphics.Bitmap;
+import android.os.RemoteException;
+import ayzf.darkqq.qqbot.msg.Messenger;
+
+/**
+ * @Author MCSQNXA
+ * @CreateTime 2022-06-25 09:04:06
+ * @Description 插件回调
+ */
+public interface PluginBinderHandler {
+
+    void onLoad() throws RemoteException;
+
+    void onUnLoad() throws RemoteException;
+
+    void onMsgHandler(Messenger messenger) throws RemoteException;
+
+    Bitmap icon() throws RemoteException;
+
+    String name() throws RemoteException;
+
+    String info() throws RemoteException;
+
+    String author() throws RemoteException;
+
+    String version() throws RemoteException;
+
+    String activity() throws RemoteException;
+
+
+}
